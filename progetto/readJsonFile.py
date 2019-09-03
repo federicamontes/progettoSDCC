4e1880e785fca0059826128effc2a481cd953525
tripps42 <federica.montesano42@gmail.com>
2019-09-03 15:44:28 +0200

    commit programma che scrive su dynamodb

---
+++ progetto/readJsonFile.py
@@ -0,0 +1,56 @@
+import json
+from pprint import pprint
+
+import boto
+import boto3
+import duo as duo
+
+
+dir = "/home/tripps42/quotes-dataset-master/quotes.json" #directory file json
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url='http://dynamodb.us-east-1.amazonaws.com')
+
+table = dynamodb.Table('Quotes')
+
+
+
+def format_object(line):
+
+    if line["Category"] == "":
+        response = table.put_item(
+            Item={
+                'Author': line["Author"],
+                'Category': "other",
+                'Popularity': str(line["Popularity"]),
+                'Quote': line["Quote"],
+                'Tags': line["Tags"]
+            }
+        )
+
+    else:
+        response = table.put_item(
+            Item={
+                'Author': line["Author"],
+                'Category': line["Category"],
+                'Popularity': str(line["Popularity"]),
+                'Quote': line["Quote"],
+                'Tags': line["Tags"]
+            }
+        )
+
+
+if (__name__ == "__main__"):
+
+
+    with open(dir) as json_data:
+        file = json.load(json_data)
+        json_data.close()
+        # pprint(file)
+
+
+    for line in file:
+        # pprint(line["Category"])
+        for i in range(0, len(file), 1000):
+            print(i)
+
+        format_object(line)
