ca2412eda6372ca5be58f0cb812194c0243115a2
tripps42 <federica.montesano42@gmail.com>
2019-09-04 12:03:25 +0200

    Add files via upload

---
+++ progetto/lambda_function.py
@@ -0,0 +1,102 @@
+
+import json
+import boto3
+from boto3.dynamodb.conditions import Key, Attr, And
+from random import randrange
+
+
+# always start with the lambda_handler
+def getQuote(author, category):
+    # make the connection to dynamodb
+    dynamodb = boto3.resource('dynamodb')
+
+    # select the table
+    table = dynamodb.Table("Quotes")
+
+
+
+    if (author == ""):
+        items = table.query(KeyConditionExpression=Key('Category').eq(category))
+        
+        lenQuotes = len(items['Items'])
+        print(lenQuotes)
+
+        if (lenQuotes == 0):
+            return "No quotes!"
+        if (lenQuotes == 1):
+            index = 0
+        else:
+            index = randrange(lenQuotes)
+    
+        return str(items['Items'][index]['Quote']) + " by " + str(items['Items'][index]['Author'])
+        
+    else:
+
+        itemsCat = table.query(KeyConditionExpression=Key('Category').eq(category))
+ 
+
+        response = []
+         
+        for i in range(len(itemsCat['Items'])):
+            if (author in itemsCat['Items'][i]['Author']):
+               print (itemsCat['Items'][i]['Quote'])
+               response.append(itemsCat['Items'][i])
+               print('\n')
+               
+        lenQuotes = len(response)
+        print(lenQuotes)
+
+        if (lenQuotes == 0):
+            return "No quotes!"
+        if (lenQuotes == 1):
+            index = 0
+        else:
+            index = randrange(lenQuotes)
+    
+        return str(response[index]['Quote']) + " by " + str(response[index]['Author'])
+
+        
+       
+    # get item from database
+    # items = table.get_item(Key={"Category": category})
+
+    lenQuotes = len(response)
+    print(lenQuotes)
+
+    if (lenQuotes == 0):
+        return "No quotes!"
+    if (lenQuotes == 1):
+        index = 0
+    else:
+        index = randrange(lenQuotes)
+
+    return str(response[index]['Quote']) + " by " + str(response[index]['Author'])
+
+
+def lambda_handler(event, context):
+    print('received request: ' + str(event))
+    # date_input = event['currentIntent']['slots']['Category']
+    # category = event['currentIntent']['slots']['Category']
+    # author = event['currentIntent']['slots']['Author']
+
+    author = "Martin"
+    category = ""
+
+    if (category == "" or author == ""):
+        quote = getQuote(author, 'other')
+    else:
+        quote = getQuote(author, category)
+
+    response = {
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": "Fulfilled",
+            "message": {
+                "contentType": "SSML",
+                "content": "The quote is: {Quote}".format(Quote=quote)
+            },
+        }
+    }
+
+    print('result = ' + str(response))
+    return response
